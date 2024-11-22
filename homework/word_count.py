68ba1f6dc88d675e82fd2b9d6a50f25b867bfb36
Felipe Muñoz <fmunoze@unal.edu.co>
2024-11-22 17:03:17 -0500

    T1

--- homework/word_count.py
+++ homework/word_count.py
@@ -1,11 +1,74 @@
 """Taller evaluable"""
 
 
+import glob
+import os
+
+import pandas as pd  # type: ignore
+
+
+def load_input(input_directory):
+    """Load text files in 'input_directory/'"""
+    #
+    # Lea los archivos de texto en la carpeta input/ y almacene el contenido en
+    # un DataFrame de Pandas. Cada línea del archivo de texto debe ser una
+    # entrada en el DataFrame.
+    #
+    files = glob.glob(f"{input_directory}/*")
+    dataframes = [
+        pd.read_csv(
+            file,
+            header=None,
+            delimiter="\t",
+            names=["line"],
+            index_col=None,
+        )
+        for file in files
+    ]
+
+    dataframe = pd.concat(dataframes, ignore_index=True)
+
+    return dataframe
+
+
+def clean_text(dataframe):
+    """Text cleaning"""
+    #
+    # Elimine la puntuación y convierta el texto a minúsculas.
+    #
+    dataframe = dataframe.copy()
+    dataframe["line"] = dataframe["line"].str.lower()
+    dataframe["line"] = (
+        dataframe["line"]
+        .str.replace(",", "")
+        .str.replace(
+            ".",
+            "",
+        )
+    )
+    return dataframe
+
+
+def count_words(dataframe):
+    """Word count"""
+
+    dataframe = dataframe.copy()
+    dataframe["line"] = dataframe["line"].str.split()
+    dataframe = dataframe.explode("line")
+    dataframe = dataframe.groupby("line").size().reset_index(name="count")
+    return dataframe
+
+
 #
 # Escriba la función job, la cual orquesta las funciones anteriores.
 #
 def run_job(input_directory, output_directory):
     """Job"""
+
+    dataframe = load_input(input_directory)
+    dataframe = clean_text(dataframe)
+    dataframe = count_words(dataframe)
+    print(dataframe)
 
 
 if __name__ == "__main__":
